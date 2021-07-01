e120b80a6cf451a07563dc697d9da4b1277ee1fe
Bhramarv021 <bhramarv021@gmail.com>
2021-07-01 22:25:24 +0530

    Add map UI with latlong 0 0 on userlistfragment activity

---
+++ app/src/main/java/com/example/chattingandlocationtrackingapp/ui/IProfile.java
@@ -0,0 +1,6 @@
+package com.example.chattingandlocationtrackingapp.ui;
+
+public interface IProfile {
+
+    void onImageSelected(int resource);
+}
---
+++ app/src/main/java/com/example/chattingandlocationtrackingapp/ui/UserListFragment.java
@@ -0,0 +1,157 @@
+package com.example.chattingandlocationtrackingapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chattingandlocationtrackingapp.R;
+import com.example.chattingandlocationtrackingapp.adapters.UserRecyclerAdapter;
+import com.example.chattingandlocationtrackingapp.models.User;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firestore.v1.MapValue;
+
+import java.util.ArrayList;
+
+import static com.example.chattingandlocationtrackingapp.Constants.MAPVIEW_BUNDLE_KEY;
+
+public class UserListFragment extends Fragment implements OnMapReadyCallback {
+
+    private static final String TAG = "UserListFragment";
+
+    //widgets
+    private RecyclerView mUserListRecyclerView;
+    private MapView mMapView;
+
+    //vars
+    private ArrayList<User> mUserList = new ArrayList<>();
+    private UserRecyclerAdapter mUserRecyclerAdapter;
+
+
+    public static UserListFragment newInstance(){
+        return new UserListFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getArguments() != null){
+            mUserList = getArguments().getParcelableArrayList(getString(R.string.intent_user_list));
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view  = inflater.inflate(R.layout.fragment_user_list, container, false);
+        mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
+        mMapView = view.findViewById(R.id.user_list_map);
+        initUserListRecyclerView();
+
+        initGoogleMap(savedInstanceState);
+
+        return view;
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState){
+        Bundle mapViewBundle = null;
+        if(savedInstanceState != null){
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+        mMapView.getMapAsync(this);
+    }
+
+    private void initUserListRecyclerView(){
+        mUserRecyclerAdapter = new UserRecyclerAdapter(mUserList);
+        mUserListRecyclerView.setAdapter(mUserRecyclerAdapter);
+        mUserListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+//        startUserLocationsRunnable(); // update user locations every 'LOCATION_UPDATE_INTERVAL'
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+//        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        map.setMyLocationEnabled(true);
+//        mGoogleMap = map;
+//        setCameraView();
+
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+//        mGoogleMap = map;
+//        addMapMarkers();
+//        mGoogleMap.setOnPolylineClickListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+//        stopLocationUpdates(); // stop updating user locations
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
